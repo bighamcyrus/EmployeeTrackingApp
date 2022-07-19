954ecc81241b53a6b7307ddd175cdcfadfce9677
Cyrus Bigham <bighamcyrus@gmail.com>
2022-07-19 13:30:19 -0700

    dependencies loaded in

---
+++ server.js
@@ -0,0 +1,5 @@
+// List of dependencies needed: 
+// const express = require('express');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const ctables = require("console.table");
